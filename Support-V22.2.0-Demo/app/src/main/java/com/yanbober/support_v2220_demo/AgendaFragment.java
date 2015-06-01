0c575a44b16e75b57b0d6b7019b2caef0db0ff44
yanbo <yanbobersky@gmail.com>
2015-06-01 17:16:32 +0800

    add support v22.2.0 demo,current is some part about 2015 google io.

---
+++ Support-V22.2.0-Demo/app/src/main/java/com/yanbober/support_v2220_demo/AgendaFragment.java
@@ -0,0 +1,25 @@
+package com.yanbober.support_v2220_demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Author       : yanbo
+ * Date         : 2015-06-01
+ * Time         : 15:09
+ * Description  :
+ */
+public class AgendaFragment extends Fragment {
+    private View mParentView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mParentView = inflater.inflate(R.layout.agenda_fragment, container, false);
+        return mParentView;
+    }
+}
